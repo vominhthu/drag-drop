2b55262f0bbf152fb53f8b8674be72bb5f6ee9fa
Thu Minh Vo <thu.minhvo@hitachids.com>
2025-02-12 01:57:53 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.scss';
+
+function App() {
+  return (
+    <>
+    <div className="dashboard-container">
+      <div className="selection-container">
+        <h2>Details</h2>
+      </div>
+      <div className="editor-container">
+        <h2>Create new Wireframe</h2>
+      </div>
+      <div className="attributes-container">
+        <h2>Attribute columns</h2>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
